141caa88abc9a4ad21d90ae4bfd6ada68683a2c4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-30 08:40:24 +1000

    specs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [c, setC] = useState(0)
+
   return (
     <div className="App">
+      <button onClick={() => setC(c => c + 1)}>Increment</button>
+      <p>Count is {c}</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
