78028fbc75797895925fe79ea36ee5636c3827c4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-06-30 12:11:16 +1000

    add 1000 specs

--- src/App_49.cy.jsx
+++ src/App_49.cy.jsx
@@ -1,12 +1,11 @@
 import App from "./App";
 
-for (let j = 1; j < 10; j++) {
-  it(`[${Cypress.spec.name}] Run #${j}: renders learn react link`, () => {
-    cy.mount(<App />);
-    cy.get("a").contains("React");
-    for (let i = 1; i < 10; i++) {
-      cy.get("button").click();
-      cy.get("p").contains(i);
-    }
-  });
-}
+it(`[${Cypress.spec.name}]: renders learn react link`, () => {
+  cy.mount(<App />);
+  cy.get("a").contains("React");
+  for (let i = 1; i < 10; i++) {
+    cy.get("button").click();
+    cy.get("p").contains(i);
+  }
+});
+
